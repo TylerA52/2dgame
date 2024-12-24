cfc2babb0745d1235362a1057668c52028b677b4
TylerA52 <tyleradams952@gmail.com>
2024-12-24 16:49:39 -0500

    adding timer class

---
+++ src/utils/timer.cpp
@@ -0,0 +1,49 @@
+#include "timer.hpp"
+
+Timer::Timer()
+    : startTime(0), pausedTime(0), totalPassed(0), running(false), paused(false) {}
+
+void Timer::start() {
+    running = true;
+    paused = false;
+    totalElapsed = 0;
+    startTime = SDL_GetTicks();
+}
+
+void Timer::pause(){
+    if (running && !paused) {
+        paused = true;
+        pausedTime = SDL_GetTicks();
+    }
+}
+
+void Timer::resume() {
+    if (paused) {
+        paused = false;
+        totalPassed += SDL_GetTicks() - pausedTime;
+    }
+}
+
+void Timer::stop() {
+    running = false;
+    paused = false;
+    totalPassed = 0;
+}
+
+Uint32 Timer::getTime() const {
+    if (running){
+        if (paused) {
+            return pausedTime - startTime;
+        } else {
+            return SDL_GetTicks() - startTime + totalPassed;
+        }
+    }
+    return 0;
+}
+
+bool Timer::isRunning() const {
+    return running;
+}
+bool Time::isPaused() const {
+    return paused;
+}
---
+++ src/utils/timer.hpp
@@ -0,0 +1,22 @@
+#pragma once 
+
+class Timer {
+
+public:
+    Timer();
+    void start();
+    void pause();
+    void resume();
+    void stop();
+    Uint32 getTime() const;
+    bool isRunning() const;
+    bool isPaused() const;
+
+private:
+    Uint32 startTime;
+    Uint32 pausedTime;
+    Uint32 totalPassed;
+
+    bool running;
+    bool paused;
+};
